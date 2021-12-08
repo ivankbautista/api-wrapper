e382f74f2db5ed75363bb846034698727ee86691
ivankbautista <ivankevinbautista@gmail.com>
2021-12-08 20:26:03 +0800

    Add in 4 endpoints into home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController < ApplicationController
 
-  def main
-    client = CoingeckoAPI::V3::Client.new
+  def index
+    client = CoingeckoApi::V3::Client.new
     @exchanges = client.get_exchanges
+    @globaldata = client.get_globaldata
+    @trending = client.search_trending
+    @companybtc = client.company_btcholdings
   end
 end
